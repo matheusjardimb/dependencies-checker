6695b7a82bef49b1ccf0796280f65e007fd76b6c
Matheus Jardim Bernardes <matheusjardimb@gmail.com>
2023-06-05 23:11:12 -0300

    Testing coverage script (#16)
    * testing coverage script
    
    * fix lint
    
    * test CI
    
    * test coverage

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,39 +1,34 @@
 const checkDependencies = require('../lib/check_dependencies');
-const process = require('process');
-const cp = require('child_process');
-const path = require('path');
-
 describe('Test invalid json files', () => {
-    it('malformed_package.json should throw SyntaxError', () => {
-        expect(
-            () => checkDependencies('tests/invalid/malformed_package.json')
-        ).toThrow(SyntaxError);
-    });
+  it('malformed_package.json should throw SyntaxError', () => {
+    expect(
+      () => checkDependencies('tests/invalid/malformed_package.json')
+    ).toThrow(SyntaxError);
+  });
 
-    it('non_existing_package.json should throw SyntaxError', () => {
-        expect(
-            () => checkDependencies('tests/invalid/non_existing_package.json')
-        ).toThrow();
-    });
+  it('non_existing_package.json should throw SyntaxError', () => {
+    expect(
+      () => checkDependencies('tests/invalid/non_existing_package.json')
+    ).toThrow();
+  });
 
-    it('package_has_caret.json should throw Error', () => {
-        expect(
-            () => checkDependencies('tests/invalid/package_has_caret.json')
-        ).toThrow();
-    });
-})
+  it('package_has_caret.json should throw Error', () => {
+    expect(
+      () => checkDependencies('tests/invalid/package_has_caret.json')
+    ).toThrow();
+  });
+});
 
 describe('Test valid json files', () => {
-    it('package_1.json should not throw error', () => {
-        expect(
-            () => checkDependencies('tests/valid/package_1.json')
-        ).not.toThrow(SyntaxError);
-    });
+  it('package_1.json should not throw error', () => {
+    expect(
+      () => checkDependencies('tests/valid/package_1.json')
+    ).not.toThrow(SyntaxError);
+  });
 
-    it('package_2.json should not throw error', () => {
-        expect(
-            () => checkDependencies('tests/valid/package_2.json')
-        ).not.toThrow(SyntaxError);
-    });
-})
-
+  it('package_2.json should not throw error', () => {
+    expect(
+      () => checkDependencies('tests/valid/package_2.json')
+    ).not.toThrow(SyntaxError);
+  });
+});
