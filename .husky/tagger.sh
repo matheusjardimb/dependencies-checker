495d8a1cda7ff5acf42df8fabedcaef99ddf9160
Matheus Jardim <matheusjardimb@gmail.com>
2023-07-05 16:18:26 -0300

    tagger

--- .husky/tagger.sh
+++ .husky/tagger.sh
@@ -1,5 +1,7 @@
 VERSION=$(python3 -c "import json;print('v'+json.load(open('package.json'))['version'])")
 
+git tag -d "$VERSION"
 git tag -a "$VERSION" -m "$VERSION"
+
 git tag -d latest
 git tag -a latest -m latest
