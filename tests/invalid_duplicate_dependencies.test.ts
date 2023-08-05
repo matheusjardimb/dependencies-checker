1b356fdae8540e4f73608ec5c809443e3141bd3e
Matheus Jardim <matheusjardimb@gmail.com>
2023-08-05 10:37:31 -0300

    testing

--- tests/invalid_duplicate_dependencies.test.ts
+++ tests/invalid_duplicate_dependencies.test.ts
@@ -1,12 +1,16 @@
 import {describe, expect, it} from '@jest/globals'
-import checkDependencies from '../src/check_dependencies'
 import {DuplicateDependencyError} from '../src/errors'
-import {get_test_file_names} from '../src/utils'
+import {get_test_file_names, setInput} from '../src/utils'
+import {packageJsonPathKey} from '../src/constants'
+import validateDependencies from '../src/run_checker'
 
 describe('Test invalid duplicate dependencies', () => {
     for (const filePath of get_test_file_names('tests/invalid_duplicate_dependencies')) {
         it(`${filePath} should throw InvalidDependencyError`, () => {
-            expect(() => checkDependencies(filePath)).toThrow(DuplicateDependencyError)
+            expect(() => {
+                setInput(packageJsonPathKey, filePath)
+                return validateDependencies()
+            }).toThrow(DuplicateDependencyError)
         })
     }
 })
--- tests/invalid_files.test.ts
+++ tests/invalid_files.test.ts
@@ -1,14 +1,18 @@
-import {describe, expect, it} from '@jest/globals'
-import checkDependencies from '../src/check_dependencies'
-import {InvalidPackageFileError} from '../src/errors'
-import {get_test_file_names} from '../src/utils'
+import { describe, expect, it } from "@jest/globals";
+import { InvalidPackageFileError } from "../src/errors";
+import { get_test_file_names, setInput } from "../src/utils";
+import { packageJsonPathKey } from "../src/constants";
+import validateDependencies from "../src/run_checker";
 
-const invalidJsonFiles = ['', 'non_existing_package.json', ...get_test_file_names('tests/invalid_files')]
+const invalidJsonFiles = ["non_existing_package.json", ...get_test_file_names("tests/invalid_files")];
 
-describe('Test invalid/malformed json files', () => {
+describe("Test invalid/malformed json files", () => {
     for (const filePath of invalidJsonFiles) {
         it(`${filePath} should throw InvalidPackageFileError`, () => {
-            expect(() => checkDependencies(filePath)).toThrow(InvalidPackageFileError)
-        })
+            expect(() => {
+                setInput(packageJsonPathKey, filePath);
+                return validateDependencies();
+            }).toThrow(InvalidPackageFileError);
+        });
     }
-})
+});
