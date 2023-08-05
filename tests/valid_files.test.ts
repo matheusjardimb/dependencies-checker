3a4b9dd2dd2aec4d05afa9c2ae5052d8e94bf8fb
Matheus Jardim <matheusjardimb@gmail.com>
2023-08-05 10:12:28 -0300

    testing

--- tests/valid_files.test.ts
+++ tests/valid_files.test.ts
@@ -1,16 +1,15 @@
-import { describe, expect, it } from "@jest/globals";
-import { get_test_file_names, setInput } from "../src/utils";
-import { packageJsonPathKey } from "../src/constants";
-import validateDependencies from "../src/run_checker";
+import {describe, expect, it} from '@jest/globals'
+import {get_test_file_names, setInput} from '../src/utils'
+import {packageJsonPathKey} from '../src/constants'
+import validateDependencies from '../src/run_checker'
 
-
-describe("Test valid json files", () => {
-    for (const filePath of get_test_file_names("tests/valid_files")) {
+describe('Test valid json files', () => {
+    for (const filePath of get_test_file_names('tests/valid_files')) {
         it(`${filePath} should not throw error`, () => {
             expect(() => {
-                setInput(packageJsonPathKey, filePath);
-                return validateDependencies();
-            }).not.toThrow();
-        });
+                setInput(packageJsonPathKey, filePath)
+                return validateDependencies()
+            }).not.toThrow()
+        })
     }
-});
+})
